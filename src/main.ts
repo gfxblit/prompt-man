6b5d44bef666f55aa8feada2c8b81b48cb50464b
Bill Chen <3580951+gfxblit@users.noreply.github.com>
2026-01-26 19:07:55 -0800

    feat: add eruda mobile debugger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { init } from './index.js';
+import eruda from 'eruda';
+
+eruda.init();
 
 if (typeof document !== 'undefined') {
   const container = document.getElementById('game-container');
